4696dffaef7f24b217dafe4d240eea0b99b3218a
Joshua Tang <tang.j1905@gmail.com>
2023-12-06 01:46:01 -0500

    add day 6 solutions

---
+++ apps/advent_of_code_2023/lib/advent_of_code/day_06.ex
@@ -0,0 +1,58 @@
+defmodule AdventOfCode2023.Day06 do
+  @moduledoc """
+  Day 6 of Advent of Code 2023.
+  """
+
+  @spec part1([String.t]) :: integer
+  def part1([times, distances | _]) do
+    time_lst =
+      times
+      |> String.trim_leading("Time:")
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    distance_lst =
+      distances
+      |> String.trim_leading("Distance:")
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    Enum.zip_with(time_lst, distance_lst, &solve/2) |> Enum.product()
+  end
+
+  @spec part2([String.t]) :: integer
+  def part2([times, distances | _]) do
+    time =
+      times
+      |> String.trim_leading("Time:")
+      |> String.replace(" ", "", global: true)
+      |> String.to_integer()
+
+    distance =
+      distances
+      |> String.trim_leading("Distance:")
+      |> String.replace(" ", "", global: true)
+      |> String.to_integer()
+
+    solve(time, distance)
+  end
+
+  # ===== Helper functions =====
+  @spec solve(integer, integer) :: integer
+  defp solve(time, dist) do
+    # This is solvable via the quadratic formula.
+    # distance < (time - time_held) * time_held
+    # 0 < -(time_held ** 2) + time * (time_held) - distance
+
+    # This is a quadratic expression with a = -1, b = time, c = -distance.
+    # x = (-b +/- sqrt(b ** 2 - 4 * a * c)) / 2 * a
+    max_time_held = (-1 * time - :math.sqrt(time * time - 4 * dist)) / -2 |> floor()
+    min_time_held = (-1 * time + :math.sqrt(time * time - 4 * dist)) / -2 |> ceil()
+
+    if (time - max_time_held) * max_time_held == dist do
+      max_time_held - min_time_held - 1   # Needs to be strictly greater...
+    else
+      max_time_held - min_time_held + 1
+    end
+  end
+end
--- apps/advent_of_code_2023/test/example_test.exs
+++ apps/advent_of_code_2023/test/example_test.exs
@@ -6,86 +6,25 @@ defmodule AdventTester do
   defp sigil_l(str, []), do:
     str |> String.split("\r\n") |> Enum.map(&String.trim/1)
 
-  import AdventOfCode2023.Day05
+  import AdventOfCode2023.Day06
 
   # @tag :skip
   test "part1" do
     input = ~l"""
-      seeds: 79 14 55 13
-
-      seed-to-soil map:
-      50 98 2
-      52 50 48
-
-      soil-to-fertilizer map:
-      0 15 37
-      37 52 2
-      39 0 15
-
-      fertilizer-to-water map:
-      49 53 8
-      0 11 42
-      42 0 7
-      57 7 4
-
-      water-to-light map:
-      88 18 7
-      18 25 70
-
-      light-to-temperature map:
-      45 77 23
-      81 45 19
-      68 64 13
-
-      temperature-to-humidity map:
-      0 69 1
-      1 0 69
-
-      humidity-to-location map:
-      60 56 37
-      56 93 4
+      Time:      7  15   30
+      Distance:  9  40  200
       """
-    assert part1(input) == 35
+
+    assert part1(input) == 288
   end
 
   # @tag :skip
   test "part2" do
     input = ~l"""
-      seeds: 79 14 55 13
-
-      seed-to-soil map:
-      50 98 2
-      52 50 48
-
-      soil-to-fertilizer map:
-      0 15 37
-      37 52 2
-      39 0 15
-
-      fertilizer-to-water map:
-      49 53 8
-      0 11 42
-      42 0 7
-      57 7 4
-
-      water-to-light map:
-      88 18 7
-      18 25 70
-
-      light-to-temperature map:
-      45 77 23
-      81 45 19
-      68 64 13
-
-      temperature-to-humidity map:
-      0 69 1
-      1 0 69
-
-      humidity-to-location map:
-      60 56 37
-      56 93 4
+      Time:      7  15   30
+      Distance:  9  40  200
       """
 
-    assert part2(input) == 46
+    assert part2(input) == 71503
   end
 end
