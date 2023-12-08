40827fc3b2484ba7fb6fcbf6566b280e84b68769
Joshua Tang <tang.j1905@gmail.com>
2023-12-08 01:12:55 -0500

    add day 8 solutions

---
+++ apps/advent_of_code_2023/lib/advent_of_code/day_08.ex
@@ -0,0 +1,59 @@
+defmodule AdventOfCode2023.Day08 do
+  @moduledoc """
+  Day 8 of Advent of Code 2023.
+  """
+
+  @spec part1([String.t]) :: integer
+  def part1([dirs, "" | maps]) do
+    tbl = to_table(maps)
+    run_len("AAA", dirs, tbl, &(&1 == "ZZZ"))
+  end
+
+  @spec part2([String.t]) :: integer
+  def part2([dirs, "" | maps]) do
+    tbl = to_table(maps)
+
+    # We take the lowest common multiple of all path lengths...
+    tbl
+    |> Map.keys()
+    |> Stream.filter(&String.ends_with?(&1, "A"))
+    |> Stream.map(fn init ->
+      run_len(init, dirs, tbl, &String.ends_with?(&1, "Z"))
+    end)
+    |> Enum.reduce(&lcm/2)
+  end
+
+  # ===== Helper functions =====
+  @type dir_table :: %{String.t => {String.t, String.t}}
+  @spec to_table([String.t]) :: dir_table
+  defp to_table(maps) do
+    for line <- maps, into: %{} do
+      <<parent :: binary-size(3), " = (",
+        left   :: binary-size(3), ", ",
+        right  :: binary-size(3), ")">> = line
+
+      {parent, {left, right}}
+    end
+  end
+
+  @spec run_len(String.t, String.t, dir_table, (String.t -> String.t)) :: integer
+  defp run_len(init, dirs, tbl, stopping_cond) do
+    dirs
+    |> String.to_charlist()
+    |> Stream.cycle()
+    |> Stream.with_index()
+    |> Enum.reduce_while(init, fn {dir, dist}, cur ->
+      if stopping_cond.(cur) do
+        {:halt, dist}
+      else
+        case dir do
+          ?L -> {:cont, tbl[cur] |> elem(0)}
+          ?R -> {:cont, tbl[cur] |> elem(1)}
+        end
+      end
+    end)
+  end
+
+  @spec lcm(integer, integer) :: integer
+  defp lcm(a, b), do: div(a * b, Integer.gcd(a, b))
+end
--- apps/advent_of_code_2023/test/example_test.exs
+++ apps/advent_of_code_2023/test/example_test.exs
@@ -3,34 +3,43 @@ defmodule AdventTester do
 
   # A convenience sigil to convert a multiline
   # heredoc to a list of lines:
-  defp sigil_l(str, []), do:
-    str |> String.split("\r\n") |> Enum.map(&String.trim/1)
+  defp sigil_l(str, []) do
+    str
+    |> String.trim_trailing()
+    |> String.split("\r\n")
+    |> Enum.map(&String.trim/1)
+  end
 
-  import AdventOfCode2023.Day07
+  import AdventOfCode2023.Day08
 
   # @tag :skip
   test "part1" do
     input = ~l"""
-      32T3K 765
-      T55J5 684
-      KK677 28
-      KTJJT 220
-      QQQJA 483
+      LLR
+
+      AAA = (BBB, BBB)
+      BBB = (AAA, ZZZ)
+      ZZZ = (ZZZ, ZZZ)
       """
 
-    assert part1(input) == 6440
+    assert part1(input) == 6
   end
 
   # @tag :skip
   test "part2" do
     input = ~l"""
-      32T3K 765
-      T55J5 684
-      KK677 28
-      KTJJT 220
-      QQQJA 483
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
       """
 
-    assert part2(input) == 5905
+    assert part2(input) == 6
   end
 end
