fcca1264434f41b684651b890e14d10ecb89c496
Joshua Tang <tang.j1905@gmail.com>
2023-12-11 00:49:15 -0500

    add day 11 solutions

---
+++ apps/advent_of_code_2023/lib/advent_of_code/day_11.ex
@@ -0,0 +1,57 @@
+defmodule AdventOfCode2023.Day11 do
+  @moduledoc """
+  Day 11 of Advent of Code 2023.
+  """
+
+  @spec part1([String.t]) :: integer
+  def part1(lines) do
+    lines
+    |> Enum.map(&String.to_charlist/1)
+    |> galaxy_positions(2)
+    |> sum_dists()
+  end
+
+  @spec part2([String.t]) :: integer
+  def part2(lines) do
+    lines
+    |> Enum.map(&String.to_charlist/1)
+    |> galaxy_positions(1000000)
+    |> sum_dists()
+  end
+
+  # ===== Helper functions =====
+  @type galaxy_map :: [[?. | ?#]]
+  @type coord :: {x :: integer, y :: integer}
+  @spec galaxy_positions(galaxy_map, integer) :: [coord]
+  defp galaxy_positions(map, expansion_factor) do
+    expand_rows = empty_rows(map)
+    expand_cols = map |> Enum.zip_with(&(&1)) |> empty_rows()
+
+    for {row, i} <- Enum.with_index(map),
+        {?#, j}  <- Enum.with_index(row) do
+      expanded_i = i + (expansion_factor - 1) * Enum.count(expand_rows, &(&1 < i))
+      expanded_j = j + (expansion_factor - 1) * Enum.count(expand_cols, &(&1 < j))
+      {expanded_i, expanded_j}
+    end
+  end
+
+  @spec empty_rows(galaxy_map) :: [integer]
+  defp empty_rows(map) do
+    map
+    |> Stream.with_index()
+    |> Stream.filter(fn {row, _i} -> Enum.all?(row, &(&1 == ?.)) end)
+    |> Enum.map(fn {_row, i} -> i end)
+  end
+
+  @spec dist(coord, coord) :: integer
+  # Chebyshev distance:
+  defp dist({i1, j1}, {i2, j2}), do:
+    abs(i2 - i1) + abs(j2 - j1)
+
+  @spec sum_dists([coord]) :: integer
+  defp sum_dists([]), do: 0
+  defp sum_dists([coord | rest]) do
+    d = rest |> Enum.map(&dist(coord, &1)) |> Enum.sum()
+    d + sum_dists(rest)
+  end
+end
--- apps/advent_of_code_2023/test/example_test.exs
+++ apps/advent_of_code_2023/test/example_test.exs
@@ -10,36 +10,42 @@ defmodule AdventTester do
     |> Enum.map(&String.trim/1)
   end
 
-  import AdventOfCode2023.Day10
+  import AdventOfCode2023.Day11
 
   # @tag :skip
   test "part1" do
     input = ~l"""
-      -L|F7
-      7S-7|
-      L|7||
-      -L-J|
-      L|-JF
+      ...#......
+      .......#..
+      #.........
+      ..........
+      ......#...
+      .#........
+      .........#
+      ..........
+      .......#..
+      #...#.....
       """
 
-    assert part1(input) == 4
+    assert part1(input) == 374
   end
 
   # @tag :skip
   test "part2" do
     input = ~l"""
-      FF7FSF7F7F7F7F7F---7
-      L|LJ||||||||||||F--J
-      FL-7LJLJ||||||LJL-77
-      F--JF--7||LJLJ7F7FJ-
-      L---JF-JLJ.||-FJLJJ7
-      |F|F-JF---7F7-L7L|7|
-      |FFJF7L7F-JF7|JL---7
-      7-L-JL7||F7|L7F-7F7|
-      L.L7LFJ|||||FJL7||LJ
-      L7JLJL-JLJLJL--JLJ.L
+      ...#......
+      .......#..
+      #.........
+      ..........
+      ......#...
+      .#........
+      .........#
+      ..........
+      .......#..
+      #...#.....
       """
 
-    assert part2(input) == 10
+    # expansion factor of 100:
+    # assert part2(input) == 8410
   end
 end
