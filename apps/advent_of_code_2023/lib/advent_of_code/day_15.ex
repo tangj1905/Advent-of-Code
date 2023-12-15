ecd04956ec0e9529f6c8e8d8c1c9287168d864ce
Joshua Tang <tang.j1905@gmail.com>
2023-12-15 00:55:43 -0500

    add day 15 solutions

---
+++ apps/advent_of_code_2023/lib/advent_of_code/day_15.ex
@@ -0,0 +1,57 @@
+defmodule AdventOfCode2023.Day15 do
+  @moduledoc """
+  Day 15 of Advent of Code 2023.
+  """
+
+  @spec part1([String.t]) :: integer
+  def part1([line]) do
+    line
+    |> String.split(",")
+    |> Stream.map(&hash_value/1)
+    |> Enum.sum()
+  end
+
+  @spec part2([String.t]) :: integer
+  def part2([line]) do
+    boxes =
+      line
+      |> String.split(",")
+      |> Enum.reduce(%{}, &place_lens/2)
+
+    for {box, lenses} <- boxes,
+        {{_, focal_len}, slot} <- Enum.with_index(lenses), reduce: 0 do
+      total -> total + (box + 1) * (slot + 1) * focal_len
+    end
+  end
+
+  # ===== Helper functions =====
+  @spec hash_value(String.t) :: integer
+  defp hash_value(input) do
+    for <<char <- input>>, reduce: 0 do
+      v -> rem((v + char) * 17, 256)
+    end
+  end
+
+  # Our boxes are represented as a map associating a box number
+  # with a list of lenses, represented as {label, focal_length}.
+
+  # Some kind of order-preserving map would be more ideal, but
+  # this should be ok too. Erlang provides some conveniences
+  # here in the :lists module.
+  @type lens :: {label :: String.t, focal_length :: integer}
+  @type box_map :: %{box :: integer => [lens]}
+
+  @spec place_lens(String.t, box_map) :: box_map
+  defp place_lens(lens, boxes) do
+    {label, lens_op} = :string.take(lens, '-=', true)
+    box_id = hash_value(label)
+
+    case lens_op do
+      "-" ->
+        Map.update(boxes, box_id, [], &:lists.keydelete(label, 1, &1))
+      "=" <> focal_len ->
+        lens = {label, String.to_integer(focal_len)}
+        Map.update(boxes, box_id, [lens], &:lists.keystore(label, 1, &1, lens))
+    end
+  end
+end
--- apps/advent_of_code_2023/test/example_test.exs
+++ apps/advent_of_code_2023/test/example_test.exs
@@ -10,41 +10,23 @@ defmodule AdventTester do
     |> Enum.map(&String.trim/1)
   end
 
-  import AdventOfCode2023.Day14
+  import AdventOfCode2023.Day15
 
   # @tag :skip
   test "part1" do
     input = ~l"""
-      O....#....
-      O.OO#....#
-      .....##...
-      OO.#O....O
-      .O.....O#.
-      O.#..O.#.#
-      ..O..#O..O
-      .......O..
-      #....###..
-      #OO..#....
+      rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
       """
 
-    assert part1(input) == 136
+    assert part1(input) == 1320
   end
 
   # @tag :skip
   test "part2" do
     input = ~l"""
-      O....#....
-      O.OO#....#
-      .....##...
-      OO.#O....O
-      .O.....O#.
-      O.#..O.#.#
-      ..O..#O..O
-      .......O..
-      #....###..
-      #OO..#....
+      rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
       """
 
-    assert part2(input) == 64
+    assert part2(input) == 145
   end
 end
