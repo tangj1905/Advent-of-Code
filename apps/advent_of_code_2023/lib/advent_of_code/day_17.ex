a1ff2d6e56b6b6aa55a4e8ed7c23ca6379731a74
Joshua Tang <tang.j1905@gmail.com>
2023-12-18 02:33:33 -0500

    switch to A* search

--- apps/advent_of_code_2023/lib/advent_of_code/day_17.ex
+++ apps/advent_of_code_2023/lib/advent_of_code/day_17.ex
@@ -3,6 +3,13 @@ defmodule AdventOfCode2023.Day17 do
   @moduledoc """
   Day 17 of Advent of Code 2023.
   """
+  @spec part1([String.t]) :: integer
+  def part1(lines), do: solve(lines, 1..3)
+
+  @spec part2([String.t]) :: integer
+  def part2(lines), do: solve(lines, 4..10)
+
+  # ===== Helper functions =====
   @type coord :: {i :: integer, j :: integer}
   @type grid  :: %{coord => loss :: integer, goal: coord}
 
@@ -11,27 +18,6 @@ defmodule AdventOfCode2023.Day17 do
     :state, location: coord, dir: {di :: -1..1, dj :: -1..1}
   )
 
-  @spec part1([String.t]) :: integer
-  def part1(lines) do
-    grid = to_grid(lines)
-    init = Enum.into([
-      {0, state(location: {0, 0}, dir: {0, 1})},
-      {0, state(location: {0, 0}, dir: {1, 0})}
-    ], Heap.min())
-    dijkstra(grid, init, MapSet.new(), 1..3)
-  end
-
-  @spec part2([String.t]) :: integer
-  def part2(lines) do
-    grid = to_grid(lines)
-    init = Enum.into([
-      {0, state(location: {0, 0}, dir: {0, 1})},
-      {0, state(location: {0, 0}, dir: {1, 0})}
-    ], Heap.min())
-    dijkstra(grid, init, MapSet.new(), 4..10)
-  end
-
-  # ===== Helper functions =====
   @spec to_grid([String.t]) :: grid
   defp to_grid(lines) do
     grid = for {row, i} <- Enum.with_index(lines),
@@ -43,14 +29,14 @@ defmodule AdventOfCode2023.Day17 do
     Map.put(grid, :goal, {height - 1, width - 1})
   end
 
-  # Good 'ol Dijkstra's algorithm.
+  # Good 'ol A* search, using Manhattan distance as the heuristic.
   # Our state is a record consisting of location & direction to face.
-  @spec dijkstra(grid, Heap.t({cost :: integer, state}), MapSet.t(state), Range.t) :: integer
-  defp dijkstra(grid, state_queue, visited, move_range) do
+  @spec a_star(grid, Heap.t({cost :: integer, state}), MapSet.t(state), Range.t) :: integer
+  defp a_star(grid, state_queue, visited, move_range) do
     {{cost, state = state(location: loc)}, state_queue} = Heap.split(state_queue)
     cond do
       loc == grid.goal -> cost
-      state in visited -> dijkstra(grid, state_queue, visited, move_range)
+      state in visited -> a_star(grid, state_queue, visited, move_range)
       true ->
         next_states =
           state
@@ -59,7 +45,7 @@ defmodule AdventOfCode2023.Day17 do
           |> Stream.map(fn {acc_cost, s} -> {cost + acc_cost, s} end)
           |> Enum.into(state_queue)
 
-        dijkstra(grid, next_states, MapSet.put(visited, state), move_range)
+        a_star(grid, next_states, MapSet.put(visited, state), move_range)
     end
   end
 
@@ -80,5 +66,27 @@ defmodule AdventOfCode2023.Day17 do
         {cost, state(location: pos, dir: {0, diff})}
       end
     end
+  end
+
+  # Using Manhattan distance, although any metric should be fine!
+  @spec dist(coord, coord) :: integer
+  defp dist({i, j}, {ii, jj}), do: abs(ii - i) + abs(jj - j)
+
+  @spec solve(input :: [String.t], Range.t) :: integer
+  defp solve(input, move_range) do
+    grid = to_grid(input)
+
+    init = [
+      {0, state(location: {0, 0}, dir: {0, 1})},
+      {0, state(location: {0, 0}, dir: {1, 0})}
+    ]
+
+    # f(n) = g(n) [cost] + h(n) [heuristic]
+    eval_fn = fn {cost, state(location: loc)} ->
+      cost + dist(loc, grid.goal)
+    end
+
+    heap = Heap.new(fn a, b -> eval_fn.(a) < eval_fn.(b) end)
+    a_star(grid, Enum.into(init, heap), MapSet.new(), move_range)
   end
 end
