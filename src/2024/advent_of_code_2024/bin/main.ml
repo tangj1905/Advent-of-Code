3f918eb7cce5d65a4d593e085910e94369672a07
Joshua Tang <tang.j1905@gmail.com>
2024-12-03 13:23:18 -0600

    modularize

--- src/2024/advent_of_code_2024/bin/main.ml
+++ src/2024/advent_of_code_2024/bin/main.ml
@@ -1,8 +1,18 @@
-(* DRIVER CODE: *)
-let input_file = "/../share/advent_of_code_2024/input01.txt"
+(* DRIVER CODE *)
+module type Solver = sig
+  val solve: string list -> string
+end
+
+module S =
+  (val (match Sys.argv with
+        | [| _; "01"; "1" |] -> (module Advent_of_code_2024.Day_01.Part_1)
+        | [| _; "01"; "2" |] -> (module Advent_of_code_2024.Day_01.Part_2)
+        | _ -> invalid_arg "Args: <day> <part>"): Solver)
+
+let input_file = Printf.sprintf "/../share/advent_of_code_2024/input%s.txt" Sys.argv.(2)
 let input_path = Filename.dirname Sys.argv.(0) ^ input_file
 
 let () =
   Core.In_channel.read_lines input_path
-  |> Advent_of_code_2024.Day_01.part2
-  |> print_int
+  |> S.solve
+  |> print_endline
--- src/2024/advent_of_code_2024/lib/day_01.ml
+++ src/2024/advent_of_code_2024/lib/day_01.ml
@@ -1,40 +1,66 @@
+open Core
+
 (* HELPER FUNCTIONS *)
+let example = {|
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+|} |> String.split_lines
+
 let rgx =
   (* Equivalent to /(\d+)\w+(\d+)/ in regex terms... *)
   let open Re in
   compile (seq [group(rep1 digit); rep1 space; group(rep1 digit)]);;
 
-let parse_line (line: string) =
-  (* Splits the given line into a pair of numbers: *)
-  let grps = Re.exec_opt rgx line in
-  Option.map
-    (fun g -> (int_of_string(Re.Group.get g 1), int_of_string(Re.Group.get g 2)))
-    grps;;
+let parse_inputs lines =
+  (* Parses the input into a pair of columns: *)
+  let parse_line line =
+    let grps = Re.exec_opt rgx line in
+    Option.map
+      ~f: (fun g -> (int_of_string(Re.Group.get g 1), int_of_string(Re.Group.get g 2)))
+      grps in
+  lines |> List.filter_map ~f: parse_line |> List.unzip 
 
-let parse_inputs (input: string list) =
-  input |> List.filter_map parse_line |> List.split 
 
 (* DRIVER CODE *)
-let part1 (lines: string list) =
-  (* Sort each input list & add the absolute differences: *)
-  let (a, b) = parse_inputs lines in
-  let std_a = List.sort compare a in
-  let std_b = List.sort compare b in
-  List.map2 (fun x y -> abs (x - y)) std_a std_b
-    |> List.fold_left (+) 0;;
-
-let part2 (lines: string list) =
-  (* Get a frequency count for elements in the second list: *)
-  let (a, b) = parse_inputs lines in
-  let freq_b = Core.Hashtbl.group (module Core.Int)
-    ~get_key: Fun.id
-    ~get_data: (Fun.const 1)
-    ~combine: (+)
-    b in
-
-  let similarity x = match Core.Hashtbl.find freq_b x with
-    | Some freq -> freq * x
-    | None -> 0 in
+module Part_1 = struct
+  (* Part 1: sort each input list & add the absolute differences *)
+  let solve (lines: string list) =
+    let (a, b) = parse_inputs lines in
+    let std_a = List.sort ~compare: compare a in
+    let std_b = List.sort ~compare: compare b in
     
-  List.map similarity a
-    |> List.fold_left (+) 0;;
+    List.map2_exn std_a std_b ~f: (fun x y -> abs (x - y))
+     |> List.fold_left ~init: 0 ~f: (+)
+     |> string_of_int;;
+
+  let%test_unit "example" =
+    [%test_eq: string] (solve example) "11"
+end
+
+
+module Part_2 = struct
+  (* Part 2: Get a frequency count for elements in the second list *)
+  let solve (lines: string list) =
+    (* Get a frequency count for elements in the second list: *)
+    let (a, b) = parse_inputs lines in
+    let freq_b = Hashtbl.group (module Int)
+      ~get_key: Fun.id
+      ~get_data: (Fun.const 1)
+      ~combine: (+)
+      b in
+  
+    let similarity x = match Hashtbl.find freq_b x with
+      | Some freq -> freq * x
+      | None -> 0 in
+      
+    List.map ~f: similarity a
+      |> List.fold_left ~init: 0 ~f: (+)
+      |> string_of_int;;
+
+  let%test_unit "example" =
+    [%test_eq: string] (solve example) "31"
+end
