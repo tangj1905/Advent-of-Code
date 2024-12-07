ebdcaabce88d76c949011752dd6763c450968fb2
tangj1905 <tang.j1905@gmail.com>
2024-12-06 23:47:52 -0500

    day 5

--- src/2024/advent_of_code_2024/bin/main.ml
+++ src/2024/advent_of_code_2024/bin/main.ml
@@ -7,12 +7,19 @@ module S =
   (val (match Sys.argv with
     | [| _; "01"; "1" |] -> (module Advent_of_code_2024.Day_01.Part_1)
     | [| _; "01"; "2" |] -> (module Advent_of_code_2024.Day_01.Part_2)
+
     | [| _; "02"; "1" |] -> (module Advent_of_code_2024.Day_02.Part_1)
     | [| _; "02"; "2" |] -> (module Advent_of_code_2024.Day_02.Part_2)
+
     | [| _; "03"; "1" |] -> (module Advent_of_code_2024.Day_03.Part_1)
     | [| _; "03"; "2" |] -> (module Advent_of_code_2024.Day_03.Part_2)
+
     | [| _; "04"; "1" |] -> (module Advent_of_code_2024.Day_04.Part_1)
     | [| _; "04"; "2" |] -> (module Advent_of_code_2024.Day_04.Part_2)
+
+    | [| _; "05"; "1" |] -> (module Advent_of_code_2024.Day_05.Part_1)
+    | [| _; "05"; "2" |] -> (module Advent_of_code_2024.Day_05.Part_2)
+
     | _ -> invalid_arg "Args: <day> <part>"): Solver)
 
 let input_file = Printf.sprintf "/../share/advent_of_code_2024/input%s.txt" Sys.argv.(1)
